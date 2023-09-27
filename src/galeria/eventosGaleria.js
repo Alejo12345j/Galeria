ef890f3f23dbbddc28ed0dcfa0a8dd65f7c5ff11
Marco Santamaria <marcoalejandrosantamaria@gmail.com>
2023-09-27 14:03:57 -0600

    Primer Commmit

---
+++ src/galeria/eventosGaleria.js
@@ -0,0 +1,40 @@
+import cerrarGaleria from "./cerrarGaleria";
+import slideClick from "./slideClick";
+import { cargarAnteriorSiguiente } from "./cargarImagen";
+import carousel from "./carousel";
+
+
+const galeria = document.getElementById('galeria');
+galeria.addEventListener('click', (e) => {
+    const boton = e.target.closest('button');
+
+    // --Cerrar Galeria
+    if (boton?.dataset?.accion === 'cerrar-galeria'){
+        cerrarGaleria();
+    }
+
+    // Carousel slide click
+    if(e.target.dataset.id){
+        slideClick(e);
+    }
+
+    // -- Siguiente Imagen
+    if(boton?.dataset?.accion === 'siguiente-imagen'){
+        cargarAnteriorSiguiente('siguiente');
+    }
+
+    // -- Anterior Imagen
+    if(boton?.dataset?.accion === 'anterior-imagen'){
+        cargarAnteriorSiguiente('anterior'); 
+    }
+
+    // -- Carousel Adelante
+    if(boton?.dataset?.accion === 'siguiente-slide'){
+         carousel('adelante');
+    }
+
+    // -- Carousel atras
+    if(boton?.dataset?.accion === 'anterior-slide'){
+        carousel('atras');
+    }
+});
